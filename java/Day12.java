77bf27dcdfeb3728796a74834de7928e4bf09acb
Rogger Matamoros <Rogger Matamoros>
2016-12-03 00:05:50 -0500

    Implemented calculate method on Student class.

--- java/Day12.java
+++ java/Day12.java
@@ -30,5 +30,25 @@ class Student extends Person {
         this.testResults = scores;
     }
 
+    char calculate() {
+        int average = 0;
+        for(int i = 0; i < this.testResults.length(); i++) {
+            average += this.testResults[i];
+        }
+        average /= this.testResults.length();
 
-}
+        if(average >= 90 && average <= 100) {
+            return 'O';
+        } else if(average >= 80 && average < 90) {
+            return 'E';
+        } else if(average >= 70 && average < 80) {
+            return 'A';
+        } else if(average >= 55 && average < 70) {
+            return 'P';
+        } else if(average >= 40 && average < 55) {
+            return 'D';
+        } else if(average < 40) {
+            return 'T';
+        }
+    }
+ }
