d57aec9490b90b2de5082bacd7d5b7dbd9b2f8c8
Rogger Matamoros <Rogger Matamoros>
2016-12-02 23:35:27 -0500

    Initial commit.

---
+++ java/Day12.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Person {
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
+
+    Person(String firstName, String lastName, 
+        int identification) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;    
+    }
+
+    public void printPerson() {
+        System.out.println(
+            "Name: " + lastName + ", " + firstName
+            + "\nID: " + idNumber);
+    }
+}
+
