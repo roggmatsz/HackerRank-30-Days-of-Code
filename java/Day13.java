488d502dc44819cd6289ab95b5b5a74b41753538
Rogger Matamoros <roggmatz@gmail.com>
2016-12-05 17:17:27 -0500

    Initial commit with code given.

---
+++ java/Day13.java
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+abstract class Book {
+    protected String title;
+    protected String author;
+
+    public Book(String t, String a) {
+        title = t;
+        author = a;
+    }
+    public abstract void display();
+}
+
+class Day13 {
+    static void main(String[] args) {
+        String title = Console.ReadLine();
+        String author = Console.ReadLine();
+        int price = Int32.Parse(Console.ReadLine());
+        
+        Book new_novel = new MyBook(title, author, price);
+        new_novel.display();
+    }
+}
