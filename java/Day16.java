81a238a8132d25763db73ec4530f79e2141539c5
Rogger Matamoros <roggmatz@gmail.com>
2016-12-05 22:46:07 -0500

    Initial given code commit.

---
+++ java/Day16.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Day16 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+    }
+}
