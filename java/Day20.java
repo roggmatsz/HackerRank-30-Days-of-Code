b2a77b99fc1c5963954ee4d2811258f5b01cfd6e
Rogger Matamoros <Rogger Matamoros>
2016-12-06 18:38:35 -0500

    Forgot to add the dimension size to a.

--- java/Day20.java
+++ java/Day20.java
@@ -5,7 +5,7 @@ public class Day20 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int a[] = new int[];
+        int a[] = new int[n];
         for(int a_i = 0; a_i < n; a_i++) {
             a[a_i] = scanner.nextInt();
         }
