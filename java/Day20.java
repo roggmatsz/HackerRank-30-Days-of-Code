71aea4b1ec096ccba5beb49648d594278bb4f5a5
Rogger Matamoros <Rogger Matamoros>
2016-12-06 18:37:54 -0500

    Initial given code commit.

---
+++ java/Day20.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Day20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a[] = new int[];
+        for(int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = scanner.nextInt();
+        }
+    }
+}
