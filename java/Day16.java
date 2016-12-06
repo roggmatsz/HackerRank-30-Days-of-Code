55a0cb27babc213d8957562779d9a4b20827d01d
Rogger Matamoros <roggmatz@gmail.com>
2016-12-05 22:49:49 -0500

    Attempt 1.

--- java/Day16.java
+++ java/Day16.java
@@ -5,5 +5,11 @@ public class Day16 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.next();
+        int parsedInt;
+        try {
+            parsedInt = Integer.parseInt(s);
+        } catch(Exception e) {
+            System.out.println("Bad String.");
+        }
     }
 }
