aa762c836ee206fe124e302cec6e9bf9829b5994
Rogger Matamoros <roggmatz@gmail.com>
2016-12-06 21:18:42 -0500

    Attempt 1.

--- python/Day1.py
+++ python/Day1.py
@@ -1,3 +1,15 @@
 i = 4
 d = 4.0
 s = 'HackerRank '
+
+secondInteger = 0
+secondDouble = 0.0
+secondString = ''
+
+secondInteger = input()
+secondDouble = input()
+secondString = input()
+
+print(i + secondInteger)
+print(d + secondDouble)
+print(s + secondString)
