9748708e94876b303e096c07ba2f69c4e27ec1c7
Rogger Matamoros <roggmatz@gmail.com>
2016-12-06 22:14:34 -0500

    Attempt 2: Forgot to append self on amIOld()

--- python/Day4.py
+++ python/Day4.py
@@ -8,7 +8,7 @@ class Person:
     def amIOld(self):
         if self.age < 13:
             print('You are young.')
-        elif age >= 13 and age < 18:
+        elif self.age >= 13 and self.age < 18:
             print('You are a teenager.')
         else:
             print('You are old')
