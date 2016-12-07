dc1a219c85ecf5925c7add4b9f5e85ced300220c
Rogger Matamoros <Rogger Matamoros>
2016-12-07 10:55:34 -0500

    Attempt 1.

--- c#/Day1.cs
+++ c#/Day1.cs
@@ -6,5 +6,17 @@ class Solution {
         int i = 4;
         double d = 4.0;
         string s = "HackerRank ";
+
+        int secInt;
+        double secDou;
+        string secStr;
+
+        secInt = (int) Console.ReadLine();
+        secDou = (double) Console.ReadLine();
+        secStr = Console.ReadLine();
+
+        Console.WriteLine(i + secInt);
+        Console.WriteLine(d + secDou);
+        Console.WriteLine(s + secStr);
     }
 }
