af2da7d8611d3472f33b8461dafdbb06253a95cd
Rogger Matamoros <roggmatz@gmail.com>
2016-12-06 21:55:07 -0500

    Attempt 1

--- python/Day3.py
+++ python/Day3.py
@@ -2,4 +2,13 @@
 
 import sys
 
-N = int(input().strip())
+n = int(input().strip())
+
+if n % 2 != 0:
+    print('Weird')
+if n % 2 == 0 and n > 1 and n < 6:
+    print('Not Weird')
+if n % 2 == 0 and n > 5 and n < 21:
+    print('Weird')
+if n % 2 == 0 and n > 20:
+    print('Not Weird')
