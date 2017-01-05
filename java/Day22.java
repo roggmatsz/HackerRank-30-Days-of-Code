82b8c403b3500ebd2c659376a760b4777e63546d
Rogger Matamoros <roggmatz@gmail.com>
2017-01-04 19:32:37 -0500

    Initial given code commit.

---
+++ java/Day22.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left, right;
+    int data;
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+class Solution {
+    public static int getHeight(Node root) {
+        
+    }
+}
